ad8a56e371888caf6e7224271557912ff902e3d6
Silpi Sulekha Sahoo <Silpi.Sahoo@ATMECS.CORP>
2022-12-01 20:23:38 +0530

    day2javascriptcode

---
+++ Day2/test1.js
@@ -0,0 +1,13 @@
+console.log("Hello World")
+console.log("hi")
+var a=4
+console.log(a)
+console.log(typeof(a))
+var b=123.6
+console.log(typeof(b))
+var c="Silpi"
+console.log(typeof(c))
+var required = true
+console.log(typeof(required))
+var c = a+b
+console.log(c)
---
+++ Day2/test2.js
@@ -0,0 +1,21 @@
+const flag = true
+if(flag){
+console.log("condition not satisfied")
+}
+else{
+    console.log(flag)
+    console.log("condition not satisfied")
+}
+let i=0
+while(i>10){
+    i++
+    console.log(i)
+}
+do{
+    i++
+}while(i>10);
+    console.log(i)
+
+    
+
+
