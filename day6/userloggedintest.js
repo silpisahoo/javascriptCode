c611eaa51daa78d7f45aed17cab59776eed53132
Silpi Sulekha Sahoo <Silpi.Sahoo@ATMECS.CORP>
2022-12-06 12:12:29 +0530

    day6 work

---
+++ day6/userloggedintest.js
@@ -0,0 +1,24 @@
+const uid = "abc1234";
+var fullname = "silpi";
+var email = "silpi@gmail.com";
+var password = "silpi@12";
+var confirmpaswword = "silpi@12";
+var state = "telengana";
+var country ="india";
+console.log("......")
+console.log(uid);
+console.log("Full Name is:" ,fullname )
+console.log(email)
+console.log(password)
+console.log(confirmpaswword)
+console.log(state)
+console.log(country)
+
+console.log(`
+With Unique Id = ${uid}
+User is = ${fullname}
+Email Id is = ${email}
+Password is = ${password}
+State of the user is =${state}
+Country of the user Is =${country}
+`);
