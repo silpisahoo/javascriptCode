129e053893160db77f6ba46a4f7a78f0263bf388
Silpi Sulekha Sahoo <Silpi.Sahoo@ATMECS.CORP>
2022-12-07 18:33:49 +0530

    added day7 work

---
+++ Day7/forinandoftest.js
@@ -0,0 +1,15 @@
+const names = [" facebook","instagaram","youtube"];
+
+for(const n of names){
+    console.log(n);
+
+}
+const symbols ={
+    fb:"facebook",
+    insta:"instagarm",
+    yt:"youtube",
+
+};
+for(const n in symbols){
+    console.log(symbols[n]);
+}
